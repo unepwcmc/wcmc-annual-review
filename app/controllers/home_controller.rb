e58fe10a868ac99d98b1a63c2e2a9019fc8cc1f3
stacytalbot <stacymtalbot@gmail.com>
2019-04-10 15:02:41 +0100

    Add content to yml files and pull in via controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
 
   def index
+    @hero = YAML.load(File.open("#{Rails.root}/lib/data/content/home/hero.yml", 'r'))
+    @foreword = YAML.load(File.open("#{Rails.root}/lib/data/content/home/foreword.yml", 'r'))
+    @anniversary = YAML.load(File.open("#{Rails.root}/lib/data/content/home/anniversary.yml", 'r'))
+    @highlights = YAML.load(File.open("#{Rails.root}/lib/data/content/home/highlights.yml", 'r'))
+    @projects = YAML.load(File.open("#{Rails.root}/lib/data/content/home/projects.yml", 'r'))
+    @stats_people = YAML.load(File.open("#{Rails.root}/lib/data/content/home/stats-people.yml", 'r'))
+    @stats_finance = YAML.load(File.open("#{Rails.root}/lib/data/content/home/stats-finance.yml", 'r'))
+    @future = YAML.load(File.open("#{Rails.root}/lib/data/content/home/future.yml", 'r'))
   end
 end
