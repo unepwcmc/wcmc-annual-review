387bee315f92714292ceb65491060a0c1d676b88
Stuart Watson <stuart.watson@unep-wcmc.org>
2019-04-29 12:21:14 +0100

    Added capistrano

--- Gemfile
+++ Gemfile
@@ -50,6 +50,14 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'capistrano', '~> 3.11.0', require: false
+  gem 'capistrano-rvm',   '~> 0.1', require: false
+  gem 'capistrano-rails',   '~> 1.1', require: false
+  gem 'capistrano-bundler', '~> 1.1', require: false
+  gem 'capistrano-passenger', '~> 0.1.1', require: false
+#  gem 'capistrano-yarn'
+#  gem 'capistrano-nvm'
+#  gem 'capistrano-sidekiq'
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,69 @@
+# config valid only for current version of Capistrano
+lock "3.11.0"
+
+set :application, 'annual_review'
+
+set :repo_url, 'git@github.com:unepwcmc/wcmc-annual-review.git'
+
+set :nvm_type, :user # or :system, depends on your nvm setup
+set :nvm_node, 'v10.15.1'
+set :nvm_map_bins, %w{node npm yarn}
+
+
+set :yarn_target_path, -> { release_path.join('client') } #
+set :yarn_flags, '--production --silent --no-progress'    # default
+set :yarn_roles, :all                                     # default
+set :yarn_env_variables, {}
+
+
+set :deploy_user, 'wcmc'
+
+
+
+set :backup_path, "/home/#{fetch(:deploy_user)}/Backup"
+
+
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
+
+# Default value for :scm is :git
+set :scm, :git
+set :scm_username, "unepwcmc-read"
+
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.5.0'
+
+
+
+set :ssh_options, {
+  forward_agent: true,
+}
+
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+#set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+#set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+set :linked_files, %w{config/database.yml .env}
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'node_modules', 'client/node_modules')
+
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+set :passenger_restart_with_touch, false
