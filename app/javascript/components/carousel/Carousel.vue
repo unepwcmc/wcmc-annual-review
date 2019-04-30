b88a2d30e62575bb358a3ed57f69e9384f82790c
stacytalbot <stacymtalbot@gmail.com>
2019-04-30 17:03:59 +0100

    Foreword desktop carousel styling

--- app/javascript/components/carousel/Carousel.vue
+++ app/javascript/components/carousel/Carousel.vue
@@ -25,7 +25,6 @@
       <template v-if="showIndicators">
         <button
           v-for="slide in totalSlides"
-          :margin-size="0"
           :title="indicatorTitle(slide)"
           :aria-controls="slidesId"
           :aria-pressed="isCurrentSlide(slide)"
