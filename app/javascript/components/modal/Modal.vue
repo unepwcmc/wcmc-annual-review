07c1724a806d875b3297ae8200bac1c405508429
Joseph Burke <joseph_burke@outlook.com>
2021-05-28 13:45:08 +0100

    Disable divider image for no-image case

--- app/javascript/components/modal/Modal.vue
+++ app/javascript/components/modal/Modal.vue
@@ -15,7 +15,7 @@
             <p v-for="p in section.text" v-html="p"></p>
           </div>
 
-          <div v-if="index == 0" class="modal__bg-image item-margin--top item-margin--bottom relative" :style="{ 'background-image': `url(${data.modal.image})` }">
+          <div v-if="index == 0 && data.modal.image" class="modal__bg-image item-margin--top item-margin--bottom relative" :style="{ 'background-image': `url(${data.modal.image})` }">
             <div class="bg-image__caption modal__bg-image-caption flex flex-column gutter-left">
               <span class="bg-image__title" v-html="data.modal.image_caption" />
               <span class="bg-image__credit">{{ data.modal.image_credit }}</span>
